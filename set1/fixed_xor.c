747713880cc20c41c2b85a9fde8992a5fb61a59c
Tobias Franzen <git@mailtfr.com>
2024-01-20 11:52:24 +0100

    set 1-3: add function to score_text

--- set1/fixed_xor.c
+++ set1/fixed_xor.c
@@ -1,3 +1,4 @@
+#include "utils.h"
 
 int fixed_xor(const char *in1, const char *in2, char *result, int length)
 {
@@ -6,4 +7,37 @@ int fixed_xor(const char *in1, const char *in2, char *result, int length)
 		result[i] = in1[i] ^ in2[i];
 	}
 	return 0;
+}
+
+/*
+ * input: text
+ * Decrypt text with all possible chars and assign a score based on character frequency
+ * output: key with best score
+  */
+char score_text(const char txt[], int size)
+{
+	char scores[255];
+	char letter = 0;
+	init_byte_array(scores, sizeof(scores));
+
+	for(int key = 0; key < sizeof(scores); ++key)
+	{
+		for(int i = 0; i < size; ++i)
+		{
+			letter = txt[i] ^ key;
+			if(letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u')
+				scores[key] += 1;
+		}
+	}
+
+	int masterkey = 0;
+	for(int i = 0, max = 0; i < sizeof(scores); ++i)
+	{
+		if(scores[i] > max)
+		{
+			masterkey = i;
+			max = scores[i];
+		}
+	}
+	return masterkey;
 }
--- set1/fixed_xor.h
+++ set1/fixed_xor.h
@@ -1,4 +1,10 @@
+#ifndef __FIXED_XOR_H_
+#define __FIXED_XOR_H_
+
 /*
   Takes two equal sized buffers and XORs them
  */
 int fixed_xor(const char *in1, const char *in2, char *result, int length);
+char score_text(const char txt[], int size);
+
+#endif // __FIXED_XOR_H_
