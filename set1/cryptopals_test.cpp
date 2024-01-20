e0f67e17824fe63edae8417ebcf51cd99d9f10e7
Tobias Franzen <git@mailtfr.com>
2024-01-20 11:52:25 +0100

    Set 1.6: remove test for 'estimate_keysize'
    Not well designed test case. Remove as it is not necessary.

--- set1/cryptopals_test.cpp
+++ set1/cryptopals_test.cpp
@@ -19,15 +19,6 @@ TEST(HammingDistance, test)
     EXPECT_EQ(is, want);
 }
 
-TEST(BreakXOR, keysize)
-{
-    char in[] = "\x0e\x36\x47\xe8\x59\x2d\x35\x51\x4a\x08\x12\x43\x58\x25\x36\xed\x3d\xe6\x73\x40\x59\x00\x1e\x3f\x53\x5c\xe6\x27\x10\x32";
-    int want = 3;
-
-    int is = estimate_keysize(in, COUNT_OF(in));
-    EXPECT_EQ(is, want);
-}
-
 TEST(BreakXOR, Set1_6)
 {
     FILE *fp;
